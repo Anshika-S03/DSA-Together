2d29df2081067965325fab7fa670e9d9fe09bac4
Anshika-S03 <anshikasharmar007@gmail.com>
2025-03-30 23:48:18 +0530

    Update lcmgcd.cpp

--- Math.cpp/lcmgcd.cpp
+++ Math.cpp/lcmgcd.cpp
@@ -1,3 +1,21 @@
 //ques -
 
 //sol -
+class Solution {
+    public:
+      vector<int> lcmAndGcd(int a, int b) {
+          // code here
+          int lcm,gcd;
+          int t,u;
+          t=a;
+          u=b;
+          while(a>0 && b >0){
+          if(a>b) a = a-b;
+          else b= b-a;
+          }
+          gcd = (a > 0 ? a : b) ;
+          lcm = (t*u)/gcd;
+          
+          return {lcm,gcd};
+      }
+  };
