d17e1d2e1659e19164f56daeacce2bccbcc72789
Anshika-S03 <anshikasharmar007@gmail.com>
2025-04-03 00:43:58 +0530

    Update powof3.cpp

--- Math.cpp/powof3.cpp
+++ Math.cpp/powof3.cpp
@@ -1,1 +1,10 @@
-
+class Solution {
+    public:
+        bool isPowerOfThree(int n) {
+             if (n <= 0) return false;
+            while (n % 3 == 0) {
+                n /= 3;
+            }
+            return n == 1;
+        }
+    }; // time complexity = o(log n)
