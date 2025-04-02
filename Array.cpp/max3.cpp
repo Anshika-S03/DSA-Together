9ebd293a6a942bc55b0ba20b868509e45430c4bf
Anshika-S03 <anshikasharmar007@gmail.com>
2025-04-03 00:04:06 +0530

    Update max3.cpp

--- Array.cpp/max3.cpp
+++ Array.cpp/max3.cpp
@@ -1,1 +1,11 @@
-
+class Solution {
+    public:
+        int thirdMax(vector<int>& nums) {
+            int n,maxi;
+            set<int> numq(nums.begin(), nums.end());
+            vector <int> v(numq.begin(), numq.end());
+            n = v.size();
+             if(n<3) return v.back();
+             else return v[n-3];
+        }
+    }; // time complexity = (nlog n)
