11c2903f642a9a04b5640f4ccefaafb08c0c1e03
Anshika-S03 <anshikasharmar007@gmail.com>
2025-04-04 20:35:53 +0530

    Update pow.cpp

--- Math.cpp/pow.cpp
+++ Math.cpp/pow.cpp
@@ -1,1 +1,8 @@
-
+class Solution {
+    public:
+        double myPow(double x, int n) {
+            double power;
+            power = pow(x,n);
+            return power;
+        }
+    }; //time complexity O(log n)
