0fb95fec0fc83802e7212c943834f07b36100711
Anshika-S03 <anshikasharmar007@gmail.com>
2025-04-04 21:58:22 +0530

    Update max69.cpp

--- Math.cpp/max69.cpp
+++ Math.cpp/max69.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+    public:
+        int maximum69Number (int num) {
+        string s = to_string(num);
+        int i,n;
+        n = s.length();
+        for(i=0;i<n;i++){
+            if(s[i]=='6'){
+                s[i] ='9';
+                break;
+            }   
+        }
+        int x = stoi(s);
+        return x;
+        }
+    }; // time complexity = O(log(num))
