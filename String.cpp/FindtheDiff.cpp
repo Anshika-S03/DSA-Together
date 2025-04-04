1b799b617cb66b3509a2d8a8105637a43b0a1b0f
Anshika-S03 <anshikasharmar007@gmail.com>
2025-04-04 21:30:49 +0530

    Update FindtheDiff.cpp

--- String.cpp/FindtheDiff.cpp
+++ String.cpp/FindtheDiff.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+    public:
+        char findTheDifference(string s, string t) {
+        int x,i;
+        char c;
+          x = s.length();
+          sort(s.begin(),s.end());
+          sort(t.begin(),t.end());
+          for(i=0;i<x;i++){
+            if(t[i] != s[i]){
+                c = t[i];
+                return c;
+            }
+          }
+         return t[x];
+        }
+    };// time complexity = O(nlogn)
